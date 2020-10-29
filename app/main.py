77683ef4c8bbdbc78819cbb7e1ccc89bda72ea33
Lucas Emanuel <lucasemanuelss@hotmail.com>
2020-10-29 20:08:50 -0300

    Fix: Add headers to cors

--- app/main.py
+++ app/main.py
@@ -49,7 +49,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=[]
+    allow_headers=["Content-Type", "Accept",]
 )
 
 # Routers
