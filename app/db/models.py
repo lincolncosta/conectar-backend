6bbfc0ce4e2aaac61f0040cc943d1fda062aa237
Lincoln M. Costa <lincoln.mcosta@gmail.com>
2021-04-05 15:49:53 -0300

    Remoção da coluna de notificação da PessoaProjeto.

--- app/db/models.py
+++ app/db/models.py
@@ -249,7 +249,6 @@ class PessoaProjeto(Base):
     situacao = Column(String)
     titulo = Column(String, nullable=True)
     remunerado = Column(Boolean, nullable=False)
-    notificacao = Column(Boolean, nullable=False)
     data_criacao = Column(DateTime(timezone=True), server_default=func.now())
     data_atualizacao = Column(DateTime(timezone=True), onupdate=func.now())
 
