5e3aeee30abb5770d4ba11bdfe4d2f0646aa1a62
tekpixo <lincoln.mcosta@gmail.com>
2021-06-15 18:14:16 -0300

    BUG: Correção nas rotas de checagem diária de notificações.

--- app/main.py
+++ app/main.py
@@ -184,9 +184,9 @@ client = TestClient(app)
 @repeat_every(seconds=60*60*24)
 def test_read_main():
     print("Verificação Diária")
-    client.post("api/v1/notificacao/checagem")
-    client.post("api/v1/notificacao/pendente_idealizador")
-    client.post("api/v1/notificacao/checagem/projeto")
+    client.post("https://boraconectar.com/notificacao/checagem")
+    client.post("https://boraconectar.com/pendente_idealizador")
+    client.post("https://boraconectar.com/notificacao/checagem/projeto")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8888)
