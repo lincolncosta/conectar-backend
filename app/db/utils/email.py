ba83caf7d70946500c0f62e6e1959a087c21510a
tekpixo <lincoln.mcosta@gmail.com>
2021-07-22 22:35:05 -0300

    bugfix: correção de código não utilizado no envio de e-mails.

--- app/db/utils/email.py
+++ app/db/utils/email.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, BackgroundTasks, UploadFile, File, Form
 from sqlalchemy.orm.session import Session
 from starlette.responses import JSONResponse
 from starlette.requests import Request
-from fastapi_mail import FastMail, MessageSchema,ConnectionConfig
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
 from pydantic import BaseModel, EmailStr
 from typing import List
 from fastapi import HTTPException, status
@@ -12,32 +12,30 @@ from db.pessoa.crud import get_pessoa_by_email
 
 import os
 
-class Envs:
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    MAIL_FROM = os.getenv('MAIL_FROM')
-    MAIL_PORT = int(os.getenv('MAIL_PORT'))
-    MAIL_SERVER = os.getenv('MAIL_SERVER')
-    MAIL_FROM_NAME = os.getenv('MAIN_FROM_NAME')
+MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+MAIL_FROM = os.getenv('MAIL_FROM')
+MAIL_PORT = int(os.getenv('MAIL_PORT'))
+MAIL_SERVER = os.getenv('MAIL_SERVER')
+MAIL_FROM_NAME = os.getenv('MAIN_FROM_NAME')
 
-class EmailSchema(BaseModel):
-    email: List[EmailStr]
 
 conf = ConnectionConfig(
-    MAIL_USERNAME=Envs.MAIL_USERNAME,
-    MAIL_PASSWORD=Envs.MAIL_PASSWORD,
-    MAIL_FROM=Envs.MAIL_FROM,
-    MAIL_PORT=Envs.MAIL_PORT,
-    MAIL_SERVER=Envs.MAIL_SERVER,
-    MAIL_FROM_NAME=Envs.MAIL_FROM_NAME,
+    MAIL_USERNAME=MAIL_USERNAME,
+    MAIL_PASSWORD=MAIL_PASSWORD,
+    MAIL_FROM=MAIL_FROM,
+    MAIL_PORT=MAIL_PORT,
+    MAIL_SERVER=MAIL_SERVER,
+    MAIL_FROM_NAME=MAIL_FROM_NAME,
     MAIL_TLS=True,
     MAIL_SSL=False,
     USE_CREDENTIALS=True,
     TEMPLATE_FOLDER='templates/'
 )
 
+
 async def envia_email_assincrono(
-    db: Session, 
+    db: Session,
     email_para: str,
 ):
 
@@ -53,9 +51,9 @@ async def envia_email_assincrono(
     message = MessageSchema(
         subject='Esqueci a senha',
         recipients=[email_para],
-        template_body={'name':pessoa.nome},
+        template_body={'name': pessoa.nome},
     )
-    
+
     fm = FastMail(conf)
     await fm.send_message(message, template_name='email.html')
 
@@ -74,4 +72,4 @@ def envia_email_bg(
     )
     fm = FastMail(conf)
     background_tasks.add_task(
-       fm.send_message, message, template_name='email.html')
+        fm.send_message, message, template_name='email.html')
